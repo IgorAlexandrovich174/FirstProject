15c7716cc15fc853a0539338a6ed860320f887f5
IgorIvanov <venomgorpo@gmail.com>
2022-03-20 13:39:27 +0500

    Создал проект FirstProject, в нем создал класс Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
+
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите числа :");
+        String str1 = input.nextLine();
+        System.out.println(str1);
+
+    }
 }
