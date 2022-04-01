8bbf986c310b520fa6180752f4af45c9a5eb607d
IgorIvanov <venomgorpo@gmail.com>
2022-04-01 16:31:11 +0500

    Сделал возможность подавать на входе римские цифры, создал для этого класс RomanNumbers, в нем 2 метода.Метод checkRomanNumber проверяет и подтверждает если числа римские. Метод romanConvert конвертирует римские числа в int.

---
+++ src/RomanNumbers.java
@@ -0,0 +1,25 @@
+public class RomanNumbers {
+
+    public String[] romanNumbers = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+
+    public boolean checkRomanNumber(String number){
+        for (int i = 0; i < romanNumbers.length; i++) {
+            String romanValue = romanNumbers[i];
+            if (romanValue.equals(number)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int romanConvert(String romanNumber) throws Exception {
+        for (int i = 0; i < romanNumbers.length; i++) {
+            String romanValue = romanNumbers[i];
+            if (romanValue.equals(romanNumber)) {
+                return i + 1;
+            }
+        }
+        throw new Exception("Число не римское!");
+    }
+}
+
